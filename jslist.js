e208e6cc0a308bae6b7c66ef55200759dc6153d4
Andrei Bayla <147172993+A-R-E-I@users.noreply.github.com>
2024-11-21 10:20:10 -0500

    Add files via upload

--- jslist.js
+++ jslist.js
@@ -1,3 +1,55 @@
-/**
- * 
- */
+window.addEventListener("load",Initialize);
+
+function Initialize()
+{
+	let carlst = ["Ford","GMC","BMW"];
+	let listelement = document.getElementById("lstorderlist");
+	let listnum = document.getElementById("lstnumberlist");
+	let listcap = document.getElementById("lstcaplist");
+	let listlow = document.getElementById("lstlowlist");
+	let listcaproman = document.getElementById("lstUpRomanlist");
+	let listlowroman = document.getElementById("lstlowRomanlist");
+	let listlength = carlst.length;
+	
+	for(let i=0; i<listlength; i++)
+	{
+		let li = document.createElement("li");
+		li.innerText = carlst[i];
+		listelement = appendChild(li);	
+	}
+	
+	for(let i=0; i<listlength; i++)
+	{
+		let li = document.createElement("li");
+		li.innerText = carlst[i];
+		listnum = appendChild(li);	
+	}
+	
+	for(let i=0; i<listlength; i++)
+	{
+		let li = document.createElement("li");
+		li.innerText = carlst[i];
+		listcap = appendChild(li);	
+	}
+	
+	for(let i=0; i<listlength; i++)
+	{
+		let li = document.createElement("li");
+		li.innerText = carlst[i];
+		listlow = appendChild(li);	
+	}
+	
+	for(let i=0; i<listlength; i++)
+	{
+		let li = document.createElement("li");
+		li.innerText = carlst[i];
+		listcaproman = appendChild(li);	
+	}
+	
+	for(let i=0; i<listlength; i++)
+	{
+		let li = document.createElement("li");
+		li.innerText = carlst[i];
+		listlowroman = appendChild(li);	
+	}
+}
